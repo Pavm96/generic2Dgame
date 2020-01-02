414f2173d01805ffec794e6550495306d30ebac6
Pavm96 <patovimuz@gmail.com>
2020-01-02 02:03:06 -0600

    Added floor tilemap and a character with horizontal movement.

---
+++ generic2Dgame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.FindObjectOfType<CharacterController2D>(); //find the character controller script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //left or A = -1, right or D = 1
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+
+    }
+}
